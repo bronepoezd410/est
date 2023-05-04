dc7bf230cb5ced740b524fe0da5562aa8870f071
Demiurg <danyushis.m.i.21@gmail.com>
2023-05-04 09:11:58 +0300

    cringe

--- vendor/addempty.php
+++ vendor/addempty.php
@@ -5,4 +5,4 @@ require_once '../config/databases.php';
 mysqli_query($induction, "INSERT INTO `products` (`id`, `name`, `description`, `brand`, `price`, `availability`, `display_size`, `display_resolution`, `processor`, `ram`, `storage`, `battery_capacity`) VALUES (NULL, 'ProductName', 'ProductDescription', 'brand', '0', '0', '0', '0', '0', '0', '0', '0')");
 
 
-header('Location: /');
+header('Location: /'); ////
